ff02621a5d5320e4db3cd09b5a1852127f5e8aeb
ALDRENOrodri <dankananh@gmail.com>
2024-09-27 08:24:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["✨️ #1 Dupe Bot"]
+const statusMessages = ["✨️ #1 STW Dupe Bot"]
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
